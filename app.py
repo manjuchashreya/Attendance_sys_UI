c0143388a9db9d60217837e0fc4bb8252be81d1d
Shreya <manjuchashreya2711@gmail.com>
2023-03-29 10:02:38 +0530

    face-recognizer model integration statis

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ from werkzeug.utils import secure_filename
 import json
 import ast
 import mysql.connector as msc
+import face_recognition
+import os, sys
+import cv2
+import numpy as np
+import math
+import random
 
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -386,11 +392,6 @@ def live():
     cap.release()
     cv2.destroyAllWindows()
     return render_template('home.html')
-                    
-            #cv2.imshow('video',img)
-            #return render_template('home.html')
-                    #current_time = now.strftime("%H-%M-%S")
-                #lnwriter.writerow([val,current_time])
 
 
 @app.route('/upload',methods=['GET','POST'])
@@ -453,6 +454,9 @@ def upload():
                 name= class_names[predictions[0]]
                 cv2.putText(cap, class_names[predictions[0]], (x+5,y-5), font, 1, (255,0,255), 2)
                 cv2.putText(cap, str(round(predictions[1],2))+"%", (x+5,y+h-5), font, 1, (255,255,0), 1)
+                #cv2.rectangle(cap, class_names[predictions[0]], (x+5,y-5), font, 1, (255,0,255), 2)
+                #cv2.rectangle(cap, str(round(predictions[1],2))+"%", (x+5,y+h-5), font, 1, (255,255,0), 1)
+                #cv2.putText(cap, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
                 if class_names[predictions[0]] in class_names.values():
                     val=str(class_names[predictions[0]])
                     print(val)
@@ -465,17 +469,255 @@ def upload():
                     if account:
                         cursor.execute('INSERT INTO attendance VALUES (NULL, % s, % s, % s,% s, % s, % s)', (current_date,current_time, sid, val,sub_name,"Present"))
                         mysql.connection.commit()
-                        cv2.destroyAllWindows()
-                        msg = 'Attendance Marked Successfully!!'
-                        flash(msg)
-                        return redirect(url_for('home'))
                     
+                    
+                #parent= "Results"
+                #directory = "model_results"
+                #path=os.path.join(parent,directory)                    
+                #os.makedirs(path,exist_ok=True)
+                #file_name = random.randint(0,900)
+                #file_name_path=f"Results/{directory}/{file_name}.jpg"
+                #cv2.imwrite(f'{file_name_path}',cap)
+
+            # Display the resulting image
+                #cv2.imshow('Face Recognition', cap)
+                
             cv2.destroyAllWindows()
-            msg = 'Attendance Not Marked!!'
+            msg = 'Attendance Marked Successfully!!'
             flash(msg)
             return redirect(url_for('home'))
+                    
+            #cv2.destroyAllWindows()
+            #msg = 'Attendance Not Marked!!'
+            #flash(msg)
+            #return redirect(url_for('home'))
 
     return redirect(url_for('home'))
+
+def face_confidence(face_distance, face_match_threshold=0.6):
+    range = (1.0 - face_match_threshold)
+    linear_val = (1.0 - face_distance) / (range * 2.0)
+
+    if face_distance > face_match_threshold:
+        return str(round(linear_val * 100, 2)) + '%'
+    else:
+        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+        return str(round(value, 2)) + '%'
+
+@app.route('/livenew',methods=['GET','POST'])
+def livenew():
+    #class_names = {1:"Shreya",2:"Shreyatwo", 3:"Muskan",4:"Kumkum", 1903064:"shreya3" , 1903032: "Muskan Gupta"}#,2:"Muskan",3:"Balaji",4:"Tejashree"} #name of people
+    now = datetime.now()
+    current_date = now.strftime("%Y-%m-%d")
+    current_time = now.strftime("%H:%M:%S")
+    print(current_date)
+    # load the model from disk
+    filename = "model1.yml"
+    
+    # f = pd.read_csv('attendance.csv')
+    # Initialize LPBH model object and load the model
+    lbph_face_classifier = cv2.face.LBPHFaceRecognizer_create()
+    lbph_face_classifier.read(filename)
+    cap=cv2.VideoCapture(0)
+    cap.set(3,640) # set Width
+    cap.set(4,480) # set Height'''
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades +"haarcascade_frontalface_default.xml")
+    sub_name="SM"
+    while True:
+        ret, img = cap.read()
+        
+        if ret == False:
+            print('Camera is not on')
+            break
+        else:
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            faces = face_classifier.detectMultiScale(gray,1.3,5)
+        #Scaling factor 1.3
+        # Minimum naber 5
+            for (x,y,w,h) in faces:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                cropped_image = img[y:y+h, x:x+w] 
+                IMG_SIZE=450
+                color_img = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+
+                #cnnModel(color_img)
+
+                predictions = lbph_face_classifier.predict(color_img)
+                #print(predictions)
+                #value=int(predictions[0])
+                #name= class_names[predictions[0]]
+                class_names= class_names_fn()
+                cv2.putText(img, str(class_names[predictions[0]]), (x+5,y-5), font, 1, (255,0,255), 2)
+                cv2.putText(img, str(round(predictions[1],2))+"%", (x+5,y+h-5), font, 1, (255,255,0), 1)
+                sid=predictions[0]
+                #print()
+                if class_names[predictions[0]] in class_names.values():
+                    val=str(class_names[predictions[0]])
+                    print(val)
+        
+                    #f.loc[value-1, 'Name'] = name
+                    #f.loc[value-1, 'Attendance'] = "Present"
+                    print("yes")
+                    
+
+                
+                    #f.to_csv('attendance.csv', index=False)
+            cv2.imshow('video',img) 
+            k = cv2.waitKey(30) & 0xff
+            if k == 27: # press 'ESC' to quit
+                break
+            
+                # If image taken reach 100, stop taking video
+    cap.release()
+    cv2.destroyAllWindows()
+    return render_template('home.html')
+
+@app.route('/face_Recognizer',methods=['GET','POST'])
+def face_Recognizer():
+    if request.method == 'POST':
+        msg = ''
+        sub_name=request.form.get('subjopt')
+        print(sub_name)  
+        # check if the post request has the file part
+        if 'studentImg' not in request.files:
+            flash('No file part')
+            print('No file part')
+            return redirect(url_for('mark_attendance_details'))
+        Uploaded_file = request.files['studentImg']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if Uploaded_file.filename == '':
+            flash('No selected file')
+            print('No selected file')
+            return redirect(url_for('mark_attendance_details'))
+        if Uploaded_file and allowed_file(Uploaded_file.filename):
+            Uploaded_file.save(os.path.join('./upload_Img', secure_filename(Uploaded_file.filename)))
+            print('Image Uploaded successfully!!')
+
+            now = datetime.now()
+            current_date = now.strftime("%Y-%m-%d")
+            current_time = now.strftime("%H:%M:%S")
+            print(current_date)
+            # load the model from disk
+            #filename = "model1.yml"
+
+            # Initialize LPBH model object and load the model
+            class FaceRecognition:
+                face_locations = []
+                face_encodings = []
+                face_names = []
+                known_face_encodings = []
+                known_face_names = []
+                process_current_frame = True
+                
+
+                def __init__(self):
+                    self.encode_faces()
+
+                def encode_faces(self):
+                    for image in os.listdir('faces'):
+                        face_image = face_recognition.load_image_file(f"faces/{image}")
+                        face_encoding = face_recognition.face_encodings(face_image)[0]
+
+                        self.known_face_encodings.append(face_encoding)
+                        self.known_face_names.append(image)
+                        #print(self.known_face_names)
+
+                def run_recognition(self):
+                    video_capture = cv2.VideoCapture(0)
+                    #filename="test.jpg"
+                    frame = cv2.imread(f"upload_img/{Uploaded_file.filename}")
+                    print("hhhhh")
+
+        #if not video_capture.isOpened():
+            #sys.exit('Video source not found...')
+
+        
+        #ret, frame = video_capture.read()
+
+            # Only process every other frame of video to save time
+                    if self.process_current_frame:
+                # Resize frame of video to 1/4 size for faster face recognition processing
+                        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                        rgb_small_frame = small_frame[:, :, ::-1]
+
+            # Find all the faces and face encodings in the current frame of video
+                        self.face_locations = face_recognition.face_locations(rgb_small_frame)
+                        self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+                        print("hello")
+            #print(self.face_locations[1:3])
+
+                        self.face_names = []
+                        for face_encoding in self.face_encodings:
+                # See if the face is a match for the known face(s)
+                            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                            name = "Unknown"
+                            confidence = '???'
+
+                # Calculate the shortest distance to face
+                            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                            #print(self.face_locations[1:2])
+
+                            best_match_index = np.argmin(face_distances)
+                            if matches[best_match_index]:
+                                name = self.known_face_names[best_match_index]
+                                confidence = face_confidence(face_distances[best_match_index])
+                                print(name,confidence)
+                    #cv2.putText(frame, name, font, 1, (255,0,255), 2)
+                    
+                    #cv2.putText(frame, confidence+"%", self.face_locations[1:2], font, 1, (255,255,0), 1)
+                   
+
+                            self.face_names.append(f'{name} ({confidence})')
+                
+
+                        self.process_current_frame = not self.process_current_frame
+
+            # Display the results
+                        for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                            top *= 4
+                            right *= 4
+                            bottom *= 4
+                            left *= 4
+
+                # Create the frame with the name
+                            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                            cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+                    parent= "Results"
+                    directory = "model_results"
+                    path=os.path.join(parent,directory)
+                    os.makedirs(path,exist_ok=True)
+                    file_name = random.randint(0,900)
+                    file_name_path=f"Results/{directory}/{file_name}.jpg"
+                    cv2.imwrite(f'{file_name_path}',frame)
+
+            # Display the resulting image
+                    cv2.imshow('Face Recognition', frame)
+
+            # Hit 'q' on the keyboard to quit!
+            
+            
+            fr = FaceRecognition()
+            fr.run_recognition()
+        # Release handle to the webcam
+        #video_capture.release()
+            cv2.destroyAllWindows()
+            msg = 'Attendance Marked Successfully!!'
+            flash(msg)
+            return redirect(url_for('home'))
+                    
+            #cv2.destroyAllWindows()
+            #msg = 'Attendance Not Marked!!'
+            #flash(msg)
+            #return redirect(url_for('home'))
+        
+    return redirect(url_for('home'))
+
 
 @app.route('/fetch_Attendance', methods=['GET','POST'])
 def fetch_Attendance():
@@ -586,4 +828,5 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
+    
     app.run(debug=True, port=5001)
